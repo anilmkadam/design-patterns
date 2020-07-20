a0087c6fa013757aea92eb93fc1419cd02d50725
tavisca-akadam <ankadam@tavisca.com>
2020-07-20 21:38:32 +0530

    Added Project structure for factory method pattern

---
+++ Creational-patterns/FactoryMethod/FactoryMethodPattern/FactoryMethodPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
